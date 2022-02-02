119fad3aecf4f226d403cc0ea1d62094937daf6d
Daniel <daniellima2297@gmail.com>
2022-02-01 22:13:36 -0300

    Initial commit

---
+++ src/components/Item.js
@@ -0,0 +1,20 @@
+
+const Items = () => {
+    let elements = []
+    
+    for (let i = 0; i < 3; i++) { 
+        elements.push(
+            <div className="item">
+                Lorem ipsum dolor sit amet, ....
+            </div>
+        )
+    }
+
+    return (
+        <div className='main'>
+            {elements}
+        </div>
+    );
+}
+ 
+export default Items;
