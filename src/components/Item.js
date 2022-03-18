2b6d4266c54d7c028e852af57be03a991b3a6272
Daniel <daniellima2297@gmail.com>
2022-03-17 22:12:05 -0300

    Listing notes

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,18 +3,29 @@
 const Items = () => {
     let elements = []
 
-    const getNotesFromLocalStorage = () =>{
-        let notes = localStorage.getItem('notes');
-        console.log(notes);
+    const addElements = (element, index, array) => {
+        elements.push(
+            <div className="item" key={element['date']}>
+                {element['name']} - {element['date']}
+            </div>
+        );
     }
 
-    for (let i = 0; i < 3; i++) { 
+    const getNotesFromLocalStorage = () =>{
+        let notesJson = localStorage.getItem('notes');
+        let notes = JSON.parse(notesJson);
+        notes.forEach(addElements);
+    }
+
+    getNotesFromLocalStorage();
+
+    /*for (let i = 0; i < 3; i++) { 
         elements.push(
             <div className="item">
                 Lorem ipsum dolor sit amet, ....
             </div>
         )
-    }
+    }*/
 
     return (
         <div className='main'>
