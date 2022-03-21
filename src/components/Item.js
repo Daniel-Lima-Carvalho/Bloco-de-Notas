cd2f3df5a795b64bbf9743ab7740dca47c2dc7f9
Daniel <daniellima2297@gmail.com>
2022-03-21 19:59:47 -0300

    cleaning code

--- src/components/Item.js
+++ src/components/Item.js
@@ -19,14 +19,6 @@ const Items = () => {
 
     getNotesFromLocalStorage();
 
-    /*for (let i = 0; i < 3; i++) { 
-        elements.push(
-            <div className="item">
-                Lorem ipsum dolor sit amet, ....
-            </div>
-        )
-    }*/
-
     return (
         <div className='main'>
             {elements}
